d5a60f2fb4f756721f0181ec80f2f52ef7364166
MANOJ CHOWDARY <manojchowdary.dev@gmail.com>
2026-05-29 15:47:37 +0530

    added array initialization examples

---
+++ 02-Arrays-Strings-Collections/Arrays/ArrayInitialization.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class ArrayInitialization {
+
+    public static void main(String[] args) {
+
+        int[] marks = {85, 90, 78, 92, 88};
+
+        System.out.println("First Element: " + marks[0]);
+        System.out.println("Second Element: " + marks[1]);
+        System.out.println("Third Element: " + marks[2]);
+    }
+}
